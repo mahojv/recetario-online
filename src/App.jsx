d59bcb4eb6eb34935e5a71d9fe56152b89323f5e
ElderSud <eldercuases@gmail.com>
2025-04-08 14:44:28 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CategoryBar from './components/CategoryBar'
+import Ingredientes from './components/Ingredientes'
 
 export default function App() {
 
   return (
     <>
-   
+   <Ingredientes/>
     </>
 
   )
---
+++ src/components/Ingredientes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Ingredientes() {
+  return (
+   <section>
+   
+        <h2 className='flex justify-center space-x-1'><img src="https://www.gourmet.cl/wp-content/themes/gourmet/img/icons/sombrero.png" alt="gormet" className='h-5'/>INGREDIENTES</h2>
+        <div>
+      
+    </div>
+    </section>
+   
+  )
+}
