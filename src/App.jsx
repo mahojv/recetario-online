9c7381cbaf6f088cf438966704031bc38e5976bf
ElderSud <eldercuases@gmail.com>
2025-04-08 16:47:46 -0500

    estilos del footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CategoryBar from './components/CategoryBar'
+import Footer from './components/Footer'
 
 export default function App() {
 
   return (
     <>
+    <Footer/>
    
     </>
 
