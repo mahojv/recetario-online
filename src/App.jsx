ea42e6615ace62b9d7887de2ae92927b7fe955ca
Miguel Angel Fuentes Alvis <miguelangelfuentesalvis@MacBook-Air-de-Miguel.local>
2025-04-10 12:44:35 -0400

    home-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-
-import React from 'react'
-
-
-
-import HomePage from './pages/HomePage'
-
-
-
+import React from "react";
+import RecipePage from "./pages/RecipePage";
+import HomePage from "./pages/HomePage";
+import { Routes, Route, Link } from "react-router";
+import HeaderBar from "./components/HeaderBar";
+import CategoryBar from "./components/CategoryBar";
 
 export default function App() {
   return (
     <>
+      <HeaderBar />
+     
 
-
-<p></p>
-
-      <HomePage/>
-
-
-
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/categorias" element={<CategoryBar />} />
+        <Route path="/receta" element={<RecipePage />} />
+       
+       
+      </Routes>
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ export default function HomePage() {
     return (
         <>
 
-            <HeaderBar />
+           
 
             <div>
                 <figure className>
