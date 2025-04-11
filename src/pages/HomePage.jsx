0ca0e2892139b2c4339df824a297745739f1cc1b
falconblade <falconbladex1@gmail.com>
2025-04-11 15:09:15 -0500

    filtros casi completo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,48 +1,41 @@
-import React, { use, useEffect } from 'react'
-import CategoryBar from '../components/CategoryBar'
-import HeaderBar from '../components/HeaderBar'
-import BarraDeFiltros from '../components/BarraDeFiltros'
-import RecipesContainer from '../components/RecipesContainer'
+import React, { use, useEffect } from "react";
+import { useState } from "react";
+import CategoryBar from "../components/CategoryBar";
+import HeaderBar from "../components/HeaderBar";
+import BarraDeFiltros from "../components/BarraDeFiltros";
+import RecipesContainer from "../components/RecipesContainer";
 
 // import sampleMeals from '../sampleMeals'
-import setData from '../hooks/setData'
+import setData from "../hooks/setData";
 
 export default function HomePage() {
-    // const meals = sampleMeals;
-    const { loading, error, response } = setData('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+  // const meals = sampleMeals;
+  const [filter, setFilter] = useState("");
+  const { loading, error, response } = setData(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${filter}`
+  );
 
+  return (
+    <>
+      <div>
+        <figure className="">
+          <img
+            src="https://www.gourmet.cl/wp-content/uploads/2025/04/Home-gourmet-desk.jpg"
+            alt=""
+          />
+        </figure>
+      </div>
 
+      <CategoryBar setFilter={setFilter} />
+      <div>
+        <BarraDeFiltros />
+        <div className="">
+          {loading && <p>Cargando recetas...</p>}
+          {error && <p className="text-red-500">Error: {error}.</p>}
 
-    return (
-        <>
-
-
-            <div>
-                <figure className>
-                    <img src="https://www.gourmet.cl/wp-content/uploads/2025/04/Home-gourmet-desk.jpg" alt="" />
-                </figure>
-
-            </div>
-
-            <CategoryBar />
-            <div>
-
-                <BarraDeFiltros />
-                <div className=''>
-
-                    {/* {loading && <p>Cargando recetas...</p>}
-                    {error && <p className="text-red-500">Error: {error}.</p>} */}
-
-                    <RecipesContainer meals={response?.meals} />
-
-
-
-
-                </div>
-
-
-            </div>
-
-        </>
-    )
+          <RecipesContainer meals={response?.meals || []} filter={filter} />
+        </div>
+      </div>
+    </>
+  );
 }
